0459975aeb82e565717e1b7d5cc306843f05f90b
Kieran Kay <kierankay@gmail.com>
2019-08-29 16:53:46 -0700

    finished all unit tests

--- test_message_views.py
+++ test_message_views.py
@@ -4,18 +4,17 @@
 #
 #    FLASK_ENV=production python -m unittest test_message_views.py
 
-
-import os
-from unittest import TestCase
-
-from models import db, connect_db, Message, User
-
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
 # before we import our app, since that will have already
 # connected to the database
 
+import os
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+from unittest import TestCase
+from models import db, connect_db, Message, User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 # Now we can import app
@@ -49,7 +48,17 @@ class MessageViewTestCase(TestCase):
                                     password="testuser",
                                     image_url=None)
 
+        self.testuser2 = User.signup(username="testuser2",
+                                    email="test2@test.com",
+                                    password="testuser",
+                                    image_url=None)
+
         db.session.commit()
+
+        testmessage = Message(user_id=self.testuser2.id, text="1238841092419")
+        db.session.add(testmessage)
+        db.session.commit()
+        self.testmessage_id = Message.query.filter(Message.text=="1238841092419").first().id
 
     def test_add_message(self):
         """Can use add a message?"""
@@ -64,10 +73,73 @@ class MessageViewTestCase(TestCase):
             # Now, that session setting is saved, so we can have
             # the rest of ours test
 
+            num_messages_before = Message.query.count()
             resp = c.post("/messages/new", data={"text": "Hello"})
-
-            # Make sure it redirects
             self.assertEqual(resp.status_code, 302)
 
-            msg = Message.query.one()
-            self.assertEqual(msg.text, "Hello")
+            num_messages_after = Message.query.count()
+            # Make sure it redirects
+            self.assertEqual(num_messages_after - num_messages_before, 1)
+    
+    def test_delete_message(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+            
+            # add the message
+            testmessage2 = Message(user_id=self.testuser.id, text="18249126739128371294")
+            db.session.add(testmessage2)
+            db.session.commit()
+
+            #check messages before and after deletion count
+            num_messages_before = Message.query.count()
+            response = c.post(f'/messages/{testmessage2.id}/delete', follow_redirects=True)
+            num_messages_after = Message.query.count()
+
+            #assertions
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(num_messages_after - num_messages_before, -1) 
+            message_search = Message.query.filter(Message.text == '18249126739128371294').one_or_none()
+            self.assertEqual(message_search, None)
+
+    def test_logged_out_delete_message(self):
+        with self.client as c:
+            testmessage2 = Message(user_id=self.testuser.id, text="18247129487124")
+            db.session.add(testmessage2)
+            db.session.commit()
+
+            num_messages_before = Message.query.count()
+            response = c.post(f'/messages/{testmessage2.id}/delete', follow_redirects=True)
+            num_messages_after = Message.query.count()
+
+            self.assertIn('<div class="alert alert-danger">Access unauthorized.</div>', str(response.data))
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(num_messages_after - num_messages_before, 0) 
+
+            message_search = Message.query.filter(Message.text == '18247129487124').one_or_none()
+            self.assertEqual(message_search.text, '18247129487124')
+
+    def test_logged_out_add_message(self):
+        with self.client as c:
+            num_messages_before = Message.query.count()
+            resp = c.post("/messages/new", data={"text": "Hello"}, follow_redirects=True)
+            num_messages_after = Message.query.count()
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('<div class="alert alert-danger">Access unauthorized.</div>', str(resp.data))
+
+            self.assertEqual(num_messages_after - num_messages_before, 0)
+
+    # def test_logged_in_delete_others_message(self):
+    #     with self.client as c:
+    #         with c.session_transaction() as sess:
+    #             sess[CURR_USER_KEY] = self.testuser.id
+
+    #         num_messages_before = Message.query.count()
+    #         response = c.post(f'/messages/{self.testmessage_id}/delete', follow_redirects=True)
+    #         num_messages_after = Message.query.count()
+
+    #         not_deleted_message = Message.query.filter(Message.text=='1238841092419').first()
+    #         self.assertIn('<div class="alert alert-danger">Access unauthorized.</div>', str(response.data))
+    #         self.assertEqual(not_deleted_message.text, '1238841092419')
+    #         self.assertEqual(num_messages_after - num_messages_before, 0)
--- test_user_views.py
+++ test_user_views.py
@@ -8,8 +8,7 @@
 # to use a different database for tests (we need to do this
 # before we import our app, since that will have already
 # connected to the database
-from flask import session
-from models import User, db, Follows
+from models import User, db, Follows, Message
 from unittest import TestCase
 from app import app
 import os
@@ -37,6 +36,7 @@ class UserViewTestCase(TestCase):
         db.session.rollback()
         User.query.delete()
         Follows.query.delete()
+        Message.query.delete()
         self.client = app.test_client()
 
         # insert a user in the db
@@ -57,11 +57,12 @@ class UserViewTestCase(TestCase):
             user_being_followed_id=self.user5.id,
             user_following_id=self.user6.id
         )
+
         db.session.add(follow)
         db.session.commit()
 
     def _login(self, username, password):
-        with app.test_client() as client:
+        with self.client as client:
             url = '/login'
             user_login = {
                 "username": username,
@@ -70,8 +71,11 @@ class UserViewTestCase(TestCase):
             response = client.post(url, data=user_login, follow_redirects=True)
         return response
 
+    def _assert200(self, response):
+        return self.assertEqual(response.status_code, 200)
+
     def test_user_creation(self):
-        with app.test_client() as client:
+        with self.client as client:
             url = '/signup'
             user4 = {
                 "username": "test4",
@@ -79,7 +83,7 @@ class UserViewTestCase(TestCase):
                 "password": "testtest"
             }
             response = client.post(url, data=user4, follow_redirects=True)
-            self.assertEqual(response.status_code, 200)
+            self._assert200(response)
 
             query = User.query.filter(User.username == 'test4').first()
             self.assertIsInstance(query, User)
@@ -90,7 +94,7 @@ class UserViewTestCase(TestCase):
                 "password": ""
             }
             response = client.post(url, data=fail_data, follow_redirects=True)
-            self.assertEqual(response.status_code, 200)
+            self._assert200(response)
             self.assertIn('Field must be at least 6 characters long.', str(
                 response.data))
 
@@ -98,29 +102,54 @@ class UserViewTestCase(TestCase):
         # successful login
         response = self._login(
             username=self.user5.username, password="testtest")
-        self.assertEqual(response.status_code, 200)
+        self._assert200(response)
         self.assertIn('Hello, test5!', str(response.data))
 
         # failed logins
         response = self._login(
             username="test500", password="testtest")
-        self.assertEqual(response.status_code, 200)
+        self._assert200(response)
         self.assertIn('Invalid credentials.', str(response.data))
 
         response = self._login(
             username="test5", password="testtttt")
-        self.assertEqual(response.status_code, 200)
+        self._assert200(response)
         self.assertIn('Invalid credentials.', str(response.data))
 
     def test_followers_page(self):
-        with app.test_client() as client:
+        with self.client as client:
             with client.session_transaction() as s:
                 s["curr_user"] = self.user5.id
-
-            self._login(username=self.user5.username, password="testtest")
 
             url = f"/users/{self.user5.id}/followers"
             response = client.get(url, follow_redirects=True)
 
-            self.assertEqual(response.status_code, 200)
+            self._assert200(response)
             self.assertIn('<p>@test6</p>', str(response.data))
+
+    def test_following_page(self):
+        with self.client as client:
+            with client.session_transaction() as s:
+                s["curr_user"] = self.user6.id
+
+            url = f"/users/{self.user6.id}/following"
+            response = client.get(url, follow_redirects=True)
+
+            self._assert200(response)
+            self.assertIn('<p>@test5</p>', str(response.data))
+
+    def test_logged_out_followers_page(self):
+        with self.client as client:
+            url = f"/users/{self.user5.id}/followers"
+            response = client.get(url, follow_redirects=True)
+
+            self._assert200(response)
+            self.assertIn('Access unauthorized.', str(response.data))
+
+    def test_logged_out_following_page(self):
+        with self.client as client:
+            url = f"/users/{self.user6.id}/following"
+            response = client.get(url, follow_redirects=True)
+
+            self._assert200(response)
+            self.assertIn('Access unauthorized.', str(response.data))
